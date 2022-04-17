6429c708c9cd6eb4bf84211d6197786b1ccc6f6f
wak10 <chiba.r25.taka@gmail.com>
2022-04-17 17:44:12 +0900

    Initial Commit

---
+++ Shared/MyMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyMapApp.swift
+//  Shared
+//
+//  Created by Takahiro Chiba on 2022/04/17.
+//
+
+import SwiftUI
+
+@main
+struct MyMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
