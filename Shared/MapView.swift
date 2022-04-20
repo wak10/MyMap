92da98c9b972a1068452d949e1fba346ec3cd4fb
wak10 <chiba.r25.taka@gmail.com>
2022-04-20 23:10:15 +0900

    [add]ピン機能

--- Shared/MapView.swift
+++ Shared/MapView.swift
@@ -23,6 +23,23 @@ struct MapView: UIViewRepresentable {
         
         //入力された文字をデバッグエリアに表示
         print(searchkey)
+        
+        //MKPointAnnotationインスタンスを取得してピンを生成
+        let pin = MKPointAnnotation()
+        
+        //ピンの置く場所に緯度経度を設定
+        pin.coordinate = targetCoordinate
+        //ピンのタイトルを設定
+        pin.title = searchkey
+        //ピンを地図に置く
+        uiView.addAnnotation(pin)
+        //緯度経度を中心に半径500mの範囲を表示
+        uiView.region = MKCoordinateRegion(
+            center: targetCoordinate,
+            latitudinalMeters: 500.0,
+            longitudinalMeters: 500.0
+            )
+        
     }
 }
 
