06850b76dbda8be66ae4d444b1700b385e45b136
wak10 <chiba.r25.taka@gmail.com>
2022-04-21 20:06:29 +0900

    [add]ピン機能

--- Shared/MapView.swift
+++ Shared/MapView.swift
@@ -24,22 +24,39 @@ struct MapView: UIViewRepresentable {
         //入力された文字をデバッグエリアに表示
         print(searchkey)
         
-        //MKPointAnnotationインスタンスを取得してピンを生成
-        let pin = MKPointAnnotation()
+        //CLGeocoderインスタンス取得
+        let geocoder = CLGeocoder()
         
-        //ピンの置く場所に緯度経度を設定
-        pin.coordinate = targetCoordinate
-        //ピンのタイトルを設定
-        pin.title = searchkey
-        //ピンを地図に置く
-        uiView.addAnnotation(pin)
-        //緯度経度を中心に半径500mの範囲を表示
-        uiView.region = MKCoordinateRegion(
-            center: targetCoordinate,
-            latitudinalMeters: 500.0,
-            longitudinalMeters: 500.0
-            )
-        
+        //入力された文字から位置情報を取得
+        geocoder.geocodeAddressString(
+        searchkey ,
+        completionHandler: { (placemarks,error) in
+            //リクエストの結果が存在し、1件目の情報から位置情報を取り出す
+            if let unwarpPlacemarks = placemarks ,
+               let firstPlacemark = unwarpPlacemarks.first ,
+               let location = firstPlacemark.location{
+                
+                //位置情報から緯度経度をtargetCoordinateに取り出す
+                let targetCoordinate = location.coordinate
+                //緯度経度をデバッグエリアに表示
+                print(targetCoordinate)
+                //MKPointAnnotationインスタンスを取得してピンを生成
+                let pin = MKPointAnnotation()
+                
+                //ピンの置く場所に緯度経度を設定
+                pin.coordinate = targetCoordinate
+                //ピンのタイトルを設定
+                pin.title = searchkey
+                //ピンを地図に置く
+                uiView.addAnnotation(pin)
+                //緯度経度を中心に半径500mの範囲を表示
+                uiView.region = MKCoordinateRegion(
+                    center: targetCoordinate,
+                    latitudinalMeters: 500.0,
+                    longitudinalMeters: 500.0
+                    )
+            }
+        })
     }
 }
 
