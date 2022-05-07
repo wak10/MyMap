aed4608cd091ab4236727a5e6239a36bd24aafdf
wak10 <chiba.r25.taka@gmail.com>
2022-05-07 17:27:45 +0900

    [add]

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -8,25 +8,25 @@
 import SwiftUI
 
 struct ContentView: View {
-    typealias Body = <#type#>
     
     //入力中の文字を保持する状態変数
     @State var inputText:String = ""
     //検索キーワードを保持する状態変数
     @State var disSearchKey:String = ""
-    }
-
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
+    
+    var body:some View{
+        //垂直にレイアウト
         VStack{
             TextField("キーワードを入力してください",
                       text: $inputText , onCommit: {
+                //入力が完了したので検索キーワードに設定する
                 dispSearchKey = inputText
-                
+                //検索キーワードをデバッグエリアに出力する
                 print("入力したキーワード" + dispSearchKey)
             })
+            
+            //余白を調整
             .padding()
-        }
-    }
-}
+        }//VStackここまで
+    }//bodyここまで
+}//Content Viewここまで
