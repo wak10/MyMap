dfd5fe83f03949c3b958807462f5dca33c3b42b5
wak10 <chiba.r25.taka@gmail.com>
2022-05-07 17:41:19 +0900

    [add]

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -1,32 +1,45 @@
 //
 //  ContentView.swift
-//  MyMap (iOS)
+//  MyMap
 //
-//  Created by Takahiro Chiba on 2022/04/27.
+//  Created by Swift-Beginners on 2020/11/05.
 //
 
 import SwiftUI
 
 struct ContentView: View {
     
-    //入力中の文字を保持する状態変数
+    // 入力中の文字列を保持する状態変数
     @State var inputText:String = ""
-    //検索キーワードを保持する状態変数
-    @State var disSearchKey:String = ""
-    
-    var body:some View{
-        //垂直にレイアウト
-        VStack{
+    // 検索キーワードを保持する状態変数
+    @State var dispSearchKey:String = ""
+
+    var body: some View {
+        // 垂直にレイアウト（縦方向にレイアウト）
+        VStack {
+            // テキストフィールド（文字入力）
             TextField("キーワードを入力してください",
                       text: $inputText , onCommit: {
-                //入力が完了したので検索キーワードに設定する
+
+                // 入力が完了したので検索キーワードに設定する
                 dispSearchKey = inputText
-                //検索キーワードをデバッグエリアに出力する
-                print("入力したキーワード" + dispSearchKey)
+                
+                // 検索キーワードをデバックエリアに出力する
+                print("入力したキーワード：" + dispSearchKey)
+                
             })
-            
-            //余白を調整
+            // 余白を追加
             .padding()
-        }//VStackここまで
-    }//bodyここまで
-}//Content Viewここまで
+            
+            // マップを表示
+            MapView(searchKey: dispSearchKey)
+            
+        } // VStack ここまで
+    } // body ここまで
+} // ContentView ここまで
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
